5144fba62570f2f57c1c700b7fb82fec545c06a9
RaphaelTrovao <raphaeltrovao123@gmail.com>
2025-10-01 06:28:34 -0300

    teste

--- Sudoku.c
+++ Sudoku.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-void geradorNum(){
+int geradorNum(){
     int quad[6][6];
     
     for(int i = 0; i<6; i++){
+       // int h1 = 6;
         for(int j = 0; j<6; j++){
             int cap = 0;
-            int cap2 = 0;
-            int num = rand() % 6+1;
             
-           
+            
+            int num = rand() % 9+1;
+            
+       
             for(int a = 0; a<6; a++){    
-                if(num != quad[a][i]){
+                if(num == quad[a][i]){
                     cap++;
                     //printf("%d\n", cap);
                 }
             }
             for(int b = 0; b<6; b++){
-                if(num != quad[j][b]){
-                    cap2++;
+                if(num == quad[j][b]){
+                    cap++;
+                }
+            }
+            if(i<3){
+                for(int c = 0; c<3; c++){
+                    for(int d = 0; d<3; d++){
+                    if(num == quad[c][d]){
+                        cap++;
+                    }
+                    }
                 }
             }
             //printf("test");
-            if(cap == 6 && cap2 == 6){
+            
+            if(cap == 0){
                 quad[j][i] = num; 
+                
             } else {
                j--;
+               //printf("travou aqui");
             }
        }
        
@@ -34,13 +48,18 @@ void geradorNum(){
         //for(int k = 3; k<6; k++){
 
         //}
-    }
-    for(int n = 0; n<6; n++){
+    
+    
+    }for(int n = 0; n<6; n++){
        for(int m = 0; m<6; m++){
+        if(m==3){
+            printf("|");
+        }
          printf(" %d ", quad[m][n]);
        }
        printf("\n");
     }
+     
 }
 
 void main(){
